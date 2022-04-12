7e41ef175a3f816ae1b3a87980fc64d6bef5b11b
EnriqueHavenstein <95871479+EnriqueHavenstein@users.noreply.github.com>
2022-04-12 18:27:48 +0200

    Update tiempos.py

--- tiempos.py
+++ tiempos.py
@@ -7,6 +7,5 @@ cycles = float(input("Number of cycles: "))
 cycle_time = total_time / cycles
 cycles_per_minute = cycles / total_time * 60
 
-print(f"Cycle time: {cycle_time} seconds.")
+print(f"Cycle time: {cycle_time} seconds")
 print(f"Cycles per minute: {cycles_per_minute}")
-
