29e571fb368798b44c1e1f96d133cebf07350208
EnriqueHavenstein <95871479+EnriqueHavenstein@users.noreply.github.com>
2022-09-24 15:12:54 +0200

    Update Times.py

--- Times.py
+++ Times.py
@@ -9,3 +9,15 @@ cycles_per_minute = cycles / total_time * 60
 
 print(f"Cycle time: {cycle_time} seconds")
 print(f"Cycles per minute: {cycles_per_minute}")
+
+# Calculate pace and speed
+
+distance = int(input('Distance (m): '))
+time = int(input('Time (s): '))
+
+speed = (distance / 1000) / (time / 3600)
+
+pace = (time / 60) / (distance / 1000)  # develop a way to see the format 4'25"
+
+print('Speed = {} km/h.'.format(speed))
+print('Pace = {} min/km.'.format(pace))
